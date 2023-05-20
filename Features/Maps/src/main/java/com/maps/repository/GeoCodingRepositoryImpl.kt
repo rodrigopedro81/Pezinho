ce5a8b09b3addb838c2adb355003e46fc250d030
Rodrigo Pedro <rodrigopedro@gmail.com>
2023-05-20 18:43:53 -0300

    Setting up geocoding service

--- Features/Maps/src/main/java/com/maps/repository/GeoCodingRepositoryImpl.kt
+++ Features/Maps/src/main/java/com/maps/repository/GeoCodingRepositoryImpl.kt
@@ -2,11 +2,19 @@ package com.maps.repository
 
 import com.network.service.GeoCodingService
 import com.repositories.GeoCodingRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GeoCodingRepositoryImpl @Inject constructor(
-    private val providesAddressService: GeoCodingService
+    private val geoCodingService: GeoCodingService
 ) : GeoCodingRepository {
 
+    private val dispatcher = Dispatchers.IO
 
+    suspend fun getAddress(apiKey: String, lat: Double, lng: Double) {
+        return withContext(dispatcher) {
+            geoCodingService.getAddress(apiKey, lat, lng)
+        }
+    }
 }
--- Features/Network/src/main/java/com/network/service/GeoCodingService.kt
+++ Features/Network/src/main/java/com/network/service/GeoCodingService.kt
@@ -1,4 +1,6 @@
 package com.network.service
 
 interface GeoCodingService {
+
+    suspend fun getAddress(apiKey: String, lat: Double, lng: Double)
 }
